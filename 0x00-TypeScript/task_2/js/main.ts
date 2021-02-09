c7d25a74b0a3954c3f2bf630cb1b447b50ed0398
Feli <55117998+FeliPrado31@users.noreply.github.com>
2021-02-09 21:39:18 +0000

    :sparkles: add 6

--- 0x00-TypeScript/task_2/js/main.ts
+++ 0x00-TypeScript/task_2/js/main.ts
@@ -46,3 +46,24 @@ export function createEmployee(salary: number | string): Teacher | Director {
 
   return new Director();
 }
+
+// task 6
+export function isDirector(
+  employee: TeacherInterface | DirectorInterface
+): employee is Director {
+  return (employee as Director).workDirectorTasks !== undefined;
+}
+
+export function executeWork(
+  employee: DirectorInterface | TeacherInterface
+): string {
+  let msg;
+
+  if (isDirector(employee)) {
+    msg = employee.workDirectorTasks();
+  } else {
+    msg = employee.workTeacherTasks();
+  }
+
+  return msg;
+}

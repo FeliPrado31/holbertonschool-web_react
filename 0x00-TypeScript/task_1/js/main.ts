f3a3180dc7103cf319e22ab2256a78ac58e9ae25
Feli <55117998+FeliPrado31@users.noreply.github.com>
2021-02-09 21:00:19 +0000

    try to fix

--- 0x00-TypeScript/task_1/js/main.ts
+++ 0x00-TypeScript/task_1/js/main.ts
@@ -1,34 +1,27 @@
-/* Task 1 */
 interface Teacher {
   readonly firstName: string;
   readonly lastName: string;
   fullTimeEmployee: boolean;
   yearsOfExperience?: number;
   location: string;
-  /* eslint-disable @typescript-eslint/no-explicit-any */
   [propName: string]: any;
-  /* eslint-enable @typescript-eslint/no-explicit-any */
 }
 
-// Task 2
 interface Directors extends Teacher {
   numberOfReports: number;
 }
 
-// Task 3
-/* eslint-disable @typescript-eslint/class-name-casing */
 interface printTeacherFunction {
   (firstName: string, lastName: string): string;
 }
 
-function printFullName(firstName: string, lastName: string): string {
-  return `${firstName.charAt(0)}. ${lastName}`;
-}
-
-export const printTeacher: printTeacherFunction = printFullName;
-/* eslint-enable @typescript-eslint/class-name-casing */
-
-// task 4
+const printTeacher: printTeacherFunction = (
+  firstName: string,
+  lastName: string
+): string => {
+  const firstInitial: string = firstName.charAt(0);
+  return `${firstInitial}. ${lastName}`;
+};
 
 interface StudentClass {
   firstName: string;

83f78910bc198631c1ab463d062a61d87c17acd4
Feli <55117998+FeliPrado31@users.noreply.github.com>
2021-02-09 17:26:14 +0000

    fix

--- 0x00-TypeScript/task_0/js/main.ts
+++ 0x00-TypeScript/task_0/js/main.ts
@@ -1,4 +1,4 @@
-// Studen interface
+/* Setup interface */
 
 interface Student {
   firstName: string;
@@ -7,28 +7,27 @@ interface Student {
   location: string;
 }
 
-// Create objects
+/* Setup student objects */
 
 const student1: Student = {
-  firstName: 'Holberton',
-  lastName: 'School',
-  age: 0,
-  location: 'idk',
+  firstName: 'Frodo',
+  lastName: 'Baggins',
+  age: 23,
+  location: 'The Shire',
 };
 
 const student2: Student = {
-  firstName: 'School',
-  lastName: 'School',
-  age: 0,
-  location: 'idk',
+  firstName: 'Bilbo',
+  lastName: 'Baggins',
+  age: 44,
+  location: 'The Shire',
 };
 
-// Create two students, and create an array named studentsList containing the two variables
-const studentsList: Student[] = [];
-studentsList.push(student1);
-studentsList.push(student2);
+const arr: Student[] = [];
+arr.push(student1);
+arr.push(student2);
 
-// Using Vanilla Javascript, render a table and for each elements in the array, append a new row to the table
+/* Manipulate DOM */
 
 const body: HTMLElement = document.body;
 const table: HTMLTableElement = document.createElement('table');
@@ -37,23 +36,23 @@ const tbody: HTMLTableSectionElement = document.createElement('tbody');
 table.innerHTML = `
   <thead>
     <tr>
-      <th colspan='2'>Students</th>
+      <th colspan="2">Students</th>
     </tr>
   </thead>`;
 
 body.appendChild(table);
 table.appendChild(tbody);
 
-studentsList.forEach(({ firstName, location }: Student) => {
+arr.forEach(({ firstName, location }: Student) => {
   const tr: HTMLTableRowElement = document.createElement('tr');
-  const firstNameTable: HTMLTableDataCellElement = document.createElement('td');
-  const locationTable: HTMLTableCellElement = document.createElement('td');
+  const nameCell: HTMLTableDataCellElement = document.createElement('td');
+  const locationCell: HTMLTableCellElement = document.createElement('td');
 
-  firstNameTable.textContent = firstName;
-  locationTable.textContent = location;
+  nameCell.textContent = firstName;
+  locationCell.textContent = location;
 
-  tr.appendChild(firstNameTable);
-  tr.appendChild(locationTable);
+  tr.appendChild(nameCell);
+  tr.appendChild(locationCell);
 
   tbody.appendChild(tr);
 });

40df54cfd5c8dd3dfc9139f08471c5da4954c859
Feli <55117998+FeliPrado31@users.noreply.github.com>
2021-02-12 17:43:20 +0000

    fix js

--- 0x01-Webpack/task_1/js/dashboard_main.js
+++ 0x01-Webpack/task_1/js/dashboard_main.js
@@ -1,15 +1,24 @@
 import $ from 'jquery';
 import { debounce } from 'lodash';
 
-let count = 0;
+// HTML
 
-$('body').append(
-  '<p>Holberton Dashboard</p>',
-  '<p>Dashboard data for the students</p>',
-  '<button>Click here to get started</button>',
-  '<p id="count"></p>',
-  '<p>Copyright - Holberton School</p>'
-);
+const body = document.getElementsByTagName('body')[0];
+const p1 = document.createElement('p');
+const p2 = document.createElement('p');
+const button = document.createElement('button');
+const p3 = document.createElement('p');
+const p4 = document.createElement('p');
+
+p1.innerHTML = 'Holberton Dashboard';
+p2.innerHTML = 'Dashboard data for the students';
+button.innerHTML = 'Click here to get started';
+p3.setAttribute('id', 'count');
+p4.innerHTML = 'Copyright - Holberton School';
+
+body.append(p1, p2, button, p3, p4);
+
+let count = 0;
 
 const updateCounter = () => (count += 1);
 
